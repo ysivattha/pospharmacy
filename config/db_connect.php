67a9f0de3eccc162990b0c7b0722d0b6ce97749c
sivattha.yun@live.com <sivattha.yun@live.com>
2022-10-08 12:00:10 +0700

    v5

---
+++ config/db_connect.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servername = "127.0.0.1";
+$username = "sunpharmacyjp";
+$password = "&sunph4rmacyjp!23";
+$dbname = "sunpharmacydb";
+	
+// creating the connection
+$connect = new mysqli($servername, $username, $password, $dbname);
+
+// checking the connection
+if(!$connect->connect_error) {
+	// echo "Successfully connected";
+}
+else {
+	die("Connection Failed : " . $connect->connect_error);
+}
+?>
